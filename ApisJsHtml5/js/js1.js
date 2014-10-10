bcdf6f8fc40276c394a622a49af1b35289c2efd1
luisgiltajamar <luis.gil@tajamar365.com>
2014-10-10 10:21:22 +0200

    Primera

---
+++ ApisJsHtml5/js/js1.js
@@ -0,0 +1,12 @@
+﻿(function() {
+
+
+    var ele = document.querySelectorAll(".dibujo");
+
+    for (var i = 0; i < ele.length; i++) {
+        ele[i].style.background = "red";
+    }
+
+    
+
+})();
