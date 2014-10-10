bad03413134a86909a33eca388218de343cf84f8
luisgiltajamar <luis.gil@tajamar365.com>
2014-10-10 10:53:37 +0200

    gps

--- ApisJsHtml5/js/js1.js
+++ ApisJsHtml5/js/js1.js
@@ -1,12 +1,40 @@
-﻿(function() {
+﻿function error(err) {
+    document.querySelector("#latitud").innerHTML = "";
+    document.querySelector("#longitud").innerHTML = "";
+    document.querySelector("#error").innerHTML = "Sin acceso";
+}
+
+function posicion() {
+    if (navigator.geolocation) {
+        navigator.geolocation.watchPosition(
+            function(pos) {
+                document.querySelector("#latitud").innerHTML = pos.coords.latitude;
+                document.querySelector("#longitud").innerHTML = pos.coords.longitude;
+
+            },error
+           
+        );
+
+    } else {
+       
+    }
+
+
+}
+
+
+(function () {
 
 
     var ele = document.querySelectorAll(".dibujo");
-
+  
     for (var i = 0; i < ele.length; i++) {
         ele[i].style.background = "red";
+        
+
+        ele[i].addEventListener("click", function () { alert('click'); });
     }
 
-    
+    posicion();
 
 })();
